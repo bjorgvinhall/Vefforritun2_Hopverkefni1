420e5b142291c8dfc7a98e762756f1c7310784bd
bjorgvinhall <43784017+bjorgvinhall@users.noreply.github.com>
2019-03-12 20:28:35 +0000

    uppsetning á gagnagrunn

---
+++ insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO users
+(username, password, email, admin)
+VALUES
+('admin', '12341234', 'admin@example.com', true);
+
---
+++ setup.js
@@ -0,0 +1,44 @@
+require('dotenv').config();
+
+const fs = require('fs');
+const util = require('util');
+
+const { query } = require('./db');
+
+const connectionString = process.env.DATABASE_URL;
+
+const readFileAsync = util.promisify(fs.readFile);
+
+async function main() {
+  console.info(`Set upp gagnagrunn á ${connectionString}`);
+  // droppa töflum ef til
+  await query('DROP TABLE IF EXISTS categories');
+  await query('DROP TABLE IF EXISTS products');
+  await query('DROP TABLE IF EXISTS users');
+  await query('DROP TABLE IF EXISTS cart');
+  await query('DROP TABLE IF EXISTS cartItems');
+  console.info('Töflum eytt');
+
+  // búa til töflur út frá skema
+  try {
+    const createTable = await readFileAsync('./schema.sql');
+    await query(createTable.toString('utf8'));
+    console.info('Töflur búnar til');
+  } catch (e) {
+    console.error('Villa við að búa til töflur:', e.message);
+    return;
+  }
+
+  // bæta færslum við töflur
+  try {
+    const insert = await readFileAsync('./insert.sql');
+    await query(insert.toString('utf8'));
+    console.info('Gögnum bætt við');
+  } catch (e) {
+    console.error('Villa við að bæta gögnum við:', e.message);
+  }
+}
+
+main().catch((err) => {
+  console.error(err);
+});
